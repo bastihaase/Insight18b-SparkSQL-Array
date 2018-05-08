06ae010eee337515b3063b221e6146efb106d112
bastihaase <bhaase2909@gmail.com>
2018-05-08 09:58:23 -0700

    cleaned up streaming job

--- spark-streaming/src/main/scala/Streaming.scala
+++ spark-streaming/src/main/scala/Streaming.scala
@@ -11,7 +11,18 @@ import scala.util.{Try, Success, Failure}
 
 object Streaming {
 
+
+  /** Function that tests if a dataframe has a certain field
+    *
+    *  @param args: Array[String]   command line input, first argument determines
+    *                               whether the intersection of arrays should be
+    *                               computed via UDFs or via the internal solution
+    *                               If args(0) = "UDF" => UDF solution
+    *                               If argso(0) = anything else => internal solution
+    *
+    */
   def main(args: Array[String]): Unit = {
+
     // Start Spark session
     val spark = SparkSession
       .builder()
@@ -30,17 +41,22 @@ object Streaming {
       spark.udf.register("UDF_INTERSECTION",
         (arr1: Seq[String], arr2: Seq[String]) => if (arr1 != null && arr2 != null) arr1.intersect(arr2) else Seq(""))
 
-      val failure_query = "SELECT -1 overlap, asin FROM table"
-      var success_query: String = new String
 
+      // If the fields we want to intersect do no exist, we want to store -1 with every asin entry
+      val fields_do_not_exist_query = "SELECT -1 overlap, asin FROM table"
+
+      // If the fields buy_after_viewing and also_viewed exist, we want to compute their intersection
+      var fields_exist_query: String = new String
+      // args(0) determines, whether we choose our internal solution or UDFs
       if (args(0) == "UDF") {
-        success_query = "SELECT asin, SIZE(UDF_INTERSECTION(related.buy_after_viewing, related.also_viewed)) overlap FROM table"
+        fields_exist_query = "SELECT asin, SIZE(UDF_INTERSECTION(related.buy_after_viewing, related.also_viewed)) overlap FROM table"
       } else {
-        success_query = "SELECT asin, SIZE(ARRAY_INTERSECTION(related.buy_after_viewing, related.also_viewed)) overlap FROM table"
+        fields_exist_query = "SELECT asin, SIZE(ARRAY_INTERSECTION(related.buy_after_viewing, related.also_viewed)) overlap FROM table"
 
       }
 
 
+      // Starting Kafka connection, subscribing to topic meta
       val preferredHosts = LocationStrategies.PreferConsistent
       val topics = List("meta")
       import org.apache.kafka.common.serialization.StringDeserializer
@@ -52,34 +68,32 @@ object Streaming {
         "auto.offset.reset" -> "earliest"
       )
 
+      // starting Kafka Direct stream
       val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
-
       val dstream = KafkaUtils.createDirectStream[String, String](
         ssc,
         preferredHosts,
         ConsumerStrategies.Subscribe[String, String](topics, kafkaParams))
 
 
-
+      // main logic on how to process the input
       dstream.foreachRDD(
         rdd => {
           val dataFrame = spark.read.json(rdd.map(_.value)) //converts json to DF
 
           dataFrame.createOrReplaceTempView("table")
 
-
+          // Choose the query based on whether all relevant fields exist
           (df_has(dataFrame, "asin"), df_has(dataFrame, "related.also_viewed"), df_has(dataFrame, "related.buy_after_viewing")) match{
-            case (Success(_), Success(_), Success(_)) => save_to_mysql (spark.sql (success_query)
-            case (Success(_), _, _) => save_to_mysql(spark.sql(failure_query))
+            case (Success(_), Success(_), Success(_)) => save_to_mysql (spark.sql (fields_exist_query)
+            case (Success(_), _, _) => save_to_mysql(spark.sql(fields_do_not_exist_query))
             case _ => println("No asin database!")
           }
         })
 
 
-
       ssc.start()
       ssc.awaitTermination()
-
 
 
     } else {
@@ -89,9 +103,21 @@ object Streaming {
   }
 
 
+  /** Function that tests if a dataframe has a certain field
+    *
+    *  @param df: DataFame    dataframe which we want to test for a field
+    *  @param field: String   field name whose existence we want to verify
+    *
+    *  @return Try[DataFrame]  In case of success, DataFrame containing the field
+    */
   def df_has(df: DataFrame, field: String): Try[DataFrame] = Try(df.select(field))
 
 
+  /** Helper function that saves dataframe to MySQL database
+    *
+    *  @param df: DataFrame     dataframe to be saved
+    *
+    */
   def save_to_mysql(df: DataFrame): Unit = {
 
     //create properties object
