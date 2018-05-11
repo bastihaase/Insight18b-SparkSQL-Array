abb7bd3da29043c4923bf5182df9b70540b094d2
bastihaase <bhaase2909@gmail.com>
2018-05-10 17:14:25 -0700

    join job updated

--- sparksql_performance_tests/src/main/scala/SparkSQL_Performance_Join.scala
+++ sparksql_performance_tests/src/main/scala/SparkSQL_Performance_Join.scala
@@ -1,6 +1,8 @@
 package Performance_Tests
 import org.apache.log4j.BasicConfigurator
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.util.{Failure, Success, Try}
 
 
 /** Defines a SparkSQL job that compares performance of
@@ -34,45 +36,18 @@ object SparkSQL_Performance_Join {
     if (args.length >= 2) {
 
       // Define UDF that intersects two sequences of strings in a nullsafe way
-      spark.udf.register("UDF_INTERSECTION",
-        (arr1: Seq[String], arr2: Seq[String]) => (Option(arr1), Option(arr2)) match {
-          case (Some(x), Some(y)) => x.intersect(y)
-          case _ => Seq()
-        })
-
+      register_intersection_udf(spark)
 
       // Creates a DataFrame from json file
       val meta_df = spark.read.json("hdfs://10.0.0.10:9000/input/" + args(0))
 
+      // Apply join transformation
+      val new_df = transform_metadata(spark, meta_df, args(1))
 
-      // Create two tempViews so we run SQL join statements on them
-      meta_df.createOrReplaceTempView("m")
-
-      // Only join with subtabel of elements that have at least 4 element in buy_after_viewing
-      val meta2 = meta_df.filter("SIZE(related.buy_after_viewing) > 3")
-      meta2.createOrReplaceTempView("m2")
-
-      // Define the query based based on command line input
-      // Either use UDF or the internal solution
-
-      var query :String = new String
-
-
-      if (args(1) == "UDF") {
-        query = "SELECT m.asin FROM m LEFT JOIN m2 ON " +
-          "UDF_INTERSECTION(m.related.also_viewed, m.related.buy_after_viewing) " +
-          "== UDF_INTERSECTION(m2.related.also_viewed, m2.related.buy_after_viewing)"
-      } else {
-        query = "SELECT m.asin FROM m LEFT JOIN m2 ON " +
-          "ARRAY_INTERSECTION(m.related.also_viewed, m.related.buy_after_viewing) " +
-          "== ARRAY_INTERSECTION(m2.related.also_viewed, m2.related.buy_after_viewing)"
+      new_df match {
+        case Success(f) => f.write.format("json").save("s3n://bastian-haase-insight-18b/results")
+        case Failure(e) => println(e)
       }
-
-      val new_df = spark.sql(query)
-
-
-      // Save results to S3 in JSON format
-      new_df.write.format("json").save("s3n://bastian-haase-insight-18b/results")
 
 
     } else
@@ -80,7 +55,53 @@ object SparkSQL_Performance_Join {
         println("Missing arguments!")
       }
 
+  }
 
+  /** Helper function that applies the query to analyze the metadata from dataframe
+    * In this case, we apply a join that uses array_intersection
+    *
+    *  @param ss: SparkSession  ambient spark session
+    *  @param df: DataFrame   dataframe to be saved
+    *  @param mode : String         "UDF" if user wants to use UDF intersection, else internal intersection is used
+    *
+    *  @return :DataFrame     dataframe returned from query
+    */
+  def transform_metadata(ss: SparkSession, df: DataFrame, mode: String): Try[DataFrame] = {
+
+    // Create a tempView so we run SQL statements
+    df.createOrReplaceTempView("view")
+
+    // Only join with subtabel of elements that have at least 4 element in buy_after_viewing
+    val df2 = df.filter("SIZE(related.buy_after_viewing) > 3")
+    df2.createOrReplaceTempView("view2")
+
+    var query :String = new String
+
+    // Define query based on mode
+    if (mode == "UDF") {
+      query = "SELECT view.asin FROM view LEFT JOIN view2 ON " +
+        "UDF_INTERSECTION(view.related.also_viewed, view.related.buy_after_viewing) " +
+        "== UDF_INTERSECTION(view2.related.also_viewed, view2.related.buy_after_viewing)"
+    } else {
+      query = "SELECT view.asin FROM view LEFT JOIN view2 ON " +
+        "ARRAY_INTERSECTION(view.related.also_viewed, view.related.buy_after_viewing) " +
+        "== ARRAY_INTERSECTION(view2.related.also_viewed, view2.related.buy_after_viewing)"
+    }
+
+    Try(ss.sql(query))
+  }
+
+  /** Registering UDF to compute intersection of array
+    *
+    *  @param ss : SparkSession          SparkSession where UDF will be registered at
+    *
+    */
+  def register_intersection_udf(ss: SparkSession): Unit = {
+    ss.udf.register("UDF_INTERSECTION",
+      (arr1: Seq[String], arr2: Seq[String]) => (Option(arr1), Option(arr2)) match {
+        case (Some(x), Some(y)) => x.intersect(y)
+        case _ => Seq()
+      })
   }
 
 
