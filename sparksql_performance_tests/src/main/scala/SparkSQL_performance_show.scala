cf5b85cac15876de62c982958426f8df86362c7d
bastihaase <bhaase2909@gmail.com>
2018-05-10 17:13:38 -0700

    show job updated

--- sparksql_performance_tests/src/main/scala/SparkSQL_performance_show.scala
+++ sparksql_performance_tests/src/main/scala/SparkSQL_performance_show.scala
@@ -1,6 +1,9 @@
 package Performance_Tests
+import Performance_Tests.SparkSQL_Performance.{register_intersection_udf, transform_metadata}
 import org.apache.log4j.BasicConfigurator
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.util.{Failure, Success, Try}
 
 
 /** Defines a SparkSQL job that compares performance of
@@ -34,36 +37,20 @@ object SparkSQL_Performance_Show {
     if (args.length >= 2) {
 
       // Define UDF that intersects two sequences of strings in a nullsafe way
-      spark.udf.register("UDF_INTERSECTION",
-        (arr1: Seq[String], arr2: Seq[String]) => (Option(arr1), Option(arr2)) match {
-          case (Some(x), Some(y)) => x.intersect(y)
-          case _ => Seq()
-        })
+      register_intersection_udf(spark)
 
 
       // Creates a DataFrame from json file
       val meta_df = spark.read.json("hdfs://10.0.0.10:9000/input/" + args(0))
 
+      // Apply transformation
+      val new_df = transform_metadata(spark, meta_df, args(1))
 
-      // Create a tempView so we run SQL statements
-      meta_df.createOrReplaceTempView("meta_view")
-
-      // Define the query based based on command line input
-      // Either use UDF or the internal solution
-
-      var query :String = new String
-
-
-      if (args(1) == "UDF") {
-        query = "SELECT UDF_INTERSECTION(related.buy_after_viewing, related.also_viewed) FROM meta_view"
-      } else {
-        query = "SELECT ARRAY_INTERSECTION(related.buy_after_viewing, related.also_viewed) FROM meta_view"
+      new_df match {
+        case Success(df) => df.show  // an action to make sure execution is executed
+        case Failure(e) => println(e)
       }
 
-      val new_df = spark.sql(query)
-
-      // To force evaluation
-      new_df.show
 
     } else
       {
@@ -71,6 +58,44 @@ object SparkSQL_Performance_Show {
       }
 
 
+  }
+
+  /** Helper function that applies the query to analyze the metadata  from dataframe
+    *
+    *  @param ss: SparkSession  ambient spark session
+    *  @param df: DataFrame   dataframe to be saved
+    *  @param mode : String         "UDF" if user wants to use UDF intersection, else internal intersection is used
+    *
+    *  @return :DataFrame     dataframe returned from query
+    */
+  def transform_metadata(ss: SparkSession, df: DataFrame, mode: String): Try[DataFrame] = {
+
+    // Create a tempView so we run SQL statements
+    df.createOrReplaceTempView("view")
+
+    var query :String = new String
+
+    // Define query based on mode
+    if (mode == "UDF") {
+      query = "SELECT UDF_INTERSECTION(related.buy_after_viewing, related.also_viewed) FROM view"
+    } else {
+      query = "SELECT ARRAY_INTERSECTION(related.buy_after_viewing, related.also_viewed) FROM view"
+    }
+
+    Try(ss.sql(query))
+  }
+
+  /** Registering UDF to compute intersection of array
+    *
+    *  @param ss : SparkSession          SparkSession where UDF will be registered at
+    *
+    */
+  def register_intersection_udf(ss: SparkSession): Unit = {
+    ss.udf.register("UDF_INTERSECTION",
+      (arr1: Seq[String], arr2: Seq[String]) => (Option(arr1), Option(arr2)) match {
+        case (Some(x), Some(y)) => x.intersect(y)
+        case _ => Seq()
+      })
   }
 
 
